b8d10a7a857c86b0e6b7b86c95fa16bc8605d6d6
Júlia Jamnická <xjamnick@fi.muni.cz>
2020-12-03 15:25:12 +0100

    fixed?

--- bad_pickle.py
+++ bad_pickle.py
@@ -8,8 +8,11 @@ import subprocess
 
 # Input injection
 def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
-    subprocess.call(command, shell=True)  # a bad idea!
+	if (isalnum(filename)) {
+		command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
+    	subprocess.call(command, shell=True)  # a bad idea!
+	}
+
 
 
 # Assert statements
